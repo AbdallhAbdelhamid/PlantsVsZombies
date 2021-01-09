7092d32f2f68cae82f66888ebb0044278f925657
Ahmed M.AbdAlAty <ahmedmahmoudabdelaty@gmail.com>
2021-01-09 23:04:14 +0200

    apply

--- PlantsVsZombies.js
+++ PlantsVsZombies.js
@@ -11,10 +11,11 @@ function getFunds() {
     return (Number.parseInt(fundsElem.innerText));
 }
 var gridObjects = {
-    Home: 1
-    , Grass: 2
-    , ZombiesWall: 3
-, };
+    Home: 1,
+    Grass: 2,
+    ZombiesWall: 3,
+    LawnRemover: 4
+};
 
 function drawGrid(gridArrIn) {
     // delete old grid from screen
@@ -23,28 +24,29 @@ function drawGrid(gridArrIn) {
         // draw new grid
         if (gridArrIn[i] == gridObjects.Home) document.getElementById("Map").innerHTML += "<div class='house'></div>"
         else if (gridArrIn[i] == gridObjects.Grass) document.getElementById("Map").innerHTML += "<div class='grass'></div>"
-        else if (gridArrIn[i] == gridObjects.ZombiesWall) document.getElementById("Map").innerHTML += "<div class='Zombies'></div>"
+        else if (gridArrIn[i] == gridObjects.ZombiesWall) document.getElementById("Map").innerHTML += "<div class='Zombies'></div>";
+        else if (gridArrIn[i] == gridObjects.LawnRemover) document.getElementById("Map").innerHTML += "<div class='LawnRemover'></div>";
     }
 }
 var lane = { // refers to top position of each lane
-    LANE1: 120
-    , LANE2: 197
-    , LANE3: 274
-    , LANE4: 351
-    , LANE5: 428
-    , LANE6: 505
-    , LANE7: 582
+    LANE1: 120,
+    LANE2: 197,
+    LANE3: 274,
+    LANE4: 351,
+    LANE5: 428,
+    LANE6: 505,
+    LANE7: 582
 };
 var CardCost = {
-    peashooter1: 100
-    , peashooter2: 175
-    , peashooter3: 200
-    , sun: 50
+    peashooter1: 100,
+    peashooter2: 175,
+    peashooter3: 200,
+    sun: 50
 }
 var ShootingSpeed = {
-    slow: 2500
-    , average: 1500
-    , fast: 750
+    slow: 1500,
+    average: 900,
+    fast: 750
 }
 
 function getLane(position) {
@@ -56,8 +58,17 @@ function getLane(position) {
     if (position < lane.LANE7) return lane.LANE6
     else return lane.LANE7
 }
+function getTractorLane(position) {
+    if (position < lane.LANE2) return 0;
+    if (position < lane.LANE3) return 1;
+    if (position < lane.LANE4) return 2;
+    if (position < lane.LANE5) return 3;
+    if (position < lane.LANE6) return 4;
+    if (position < lane.LANE7) return 5;
+    else return 6;
+}
 
-function Peashooter(peashotterDiv, positionIn, shottingSpeed,grassIn) {
+function Peashooter(peashotterDiv, positionIn, shottingSpeed, grassIn) {
     this.gridObject = peashotterDiv;
     this.lane = getLane(positionIn.top);
     this.grass = grassIn;
@@ -141,8 +152,9 @@ function Sun(sunDiv, positionIn) {
         , l: positionIn.left
     };
     this.addSunInterval = setInterval(function () { // add 50 sun every fixed time 
+        if(getFunds() <400)
         changeFunds(Sun.cost);
-    }, 3000);
+    }, 10000);
     this.destroySun = function () { // stop adding sun
         clearInterval(this.addSunInterval);
     }
@@ -156,14 +168,14 @@ Sun.cost = 50;
 var pShooters = [];
 var suns = [];
 var levelOneGrid = [
-                    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                    , 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                    , 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                    , 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                    , 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                    , 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                    , 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
-                   ]
+    1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+    , 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+    , 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+    , 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+    , 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+    , 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+    , 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+]
 $(function () {
     $(".draggable").draggable({
         revert: false
@@ -196,20 +208,22 @@ $(function () {
             if (draggedClone.hasClass('peashooter') && getFunds() >= CardCost.peashooter1) {
                 enoughFunds = true;
                 changeFunds(-CardCost.peashooter1);
+                
                 draggedClone.css({
                     "background-image": "url(./images/PShootMove2.gif)"
                     , "background-color": "transparent"
                 });
-                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.slow,$(this));
+                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.slow, $(this));
                 // pShooters[pShooters.length - 1].startShooting();
             }
             else if (draggedClone.hasClass('peashooter2') && getFunds() >= CardCost.peashooter2) {
                 enoughFunds = true;
                 changeFunds(-CardCost.peashooter2);
+                
                 draggedClone.css({
                     "background-image": "url(./images/Cards/snow-pea.gif)"
                 });
-                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.average,$(this));
+                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.average, $(this));
                 //  pShooters[pShooters.length - 1].startShooting();
             }
             else if (draggedClone.hasClass('peashooter3') && getFunds() >= CardCost.peashooter3) {
@@ -218,11 +232,42 @@ $(function () {
                 draggedClone.css({
                     "background-image": "url(./images/Cards/pea-shooter2.gif)"
                 });
-                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.fast,$(this));
+                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.fast, $(this));
                 // pShooters[pShooters.length - 1].startShooting();
             }
             else if (draggedClone.hasClass('sunflower') && getFunds() >= CardCost.sun) {
                 enoughFunds = true;
+                /*
+                                enoughFunds = true;
+                                changeFunds(-CardCost.peashooter1);
+                                draggedClone.css({
+                                    "background-image": "url(./images/PShootMove2.gif)"
+                                    , "background-color": "transparent"
+                                });
+                                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.slow,$(this));
+                                // pShooters[pShooters.length - 1].startShooting();
+                            }
+                            else if (draggedClone.hasClass('peashooter2') && getFunds() >= CardCost.peashooter2) {
+                                enoughFunds = true;
+                                changeFunds(-CardCost.peashooter2);
+                                draggedClone.css({
+                                    "background-image": "url(./images/Cards/snow-pea.gif)"
+                                });
+                                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.average,$(this));
+                                //  pShooters[pShooters.length - 1].startShooting();
+                            }
+                            else if (draggedClone.hasClass('peashooter3') && getFunds() >= CardCost.peashooter3) {
+                                enoughFunds = true;
+                                changeFunds(-CardCost.peashooter3);
+                                draggedClone.css({
+                                    "background-image": "url(./images/Cards/pea-shooter2.gif)"
+                                });
+                                pShooters[pShooters.length] = new Peashooter(draggedClone, $(this).position(), ShootingSpeed.fast,$(this));
+                                // pShooters[pShooters.length - 1].startShooting();
+                            }
+                            else if (draggedClone.hasClass('sunflower') && getFunds() >= CardCost.sun) {
+                                enoughFunds = true;
+                */
                 changeFunds(-CardCost.sun);
                 suns[suns.length] = new Sun(draggedClone, $(this).position());
             }
@@ -239,14 +284,17 @@ var int1 = setInterval(function () {
         var zombieLeft = $("#" + zombie).position().left;
         pShooters.forEach(function (pShooter) {
             if (pShooter.lane == zombieLane) pShooter.startShooting(zombie);
-            if (pShooter.position.l + 50 > zombieLeft && pShooter.lane == zombieLane) pShooter.deleteObject();
-            pShooter.grass.droppable("enable");
+            if (pShooter.position.l + 50 > zombieLeft && pShooter.lane == zombieLane) {
+                pShooter.deleteObject();
+                pShooter.grass.droppable("enable");
+            }
         });
         suns.forEach(function (sun) {
             if (sun.position.l + 50 > zombieLeft && sun.lane == zombieLane) sun.deleteObject();
         });
     })
 }, 200);
+
 
 
 
@@ -272,8 +320,7 @@ function Zombie(blood, walkSpeed, zombieStyle, bountyIn) {
     this.ZombieLeftMovement = "-=10";
     this.ZombieKillBounty = bountyIn;
     //Array of Zombies allowable top positions
-    this.AllowableZombieTop = ['120px', '197px', '274px', '351px', '428px', '505px', '582px']; //margintop+marginbottom+zombie height
-    Zombie.ZombieCount++;
+     this.AllowableZombieTop = ['120px', '197px', '274px', '351px', '428px', '505px', '582px']; //margintop+marginbottom+zombie height
 }
 Zombie.ZombiesArr = []; //stor zombies ids only
 Zombie.ZombieClasses = ['ZombiesSoldier1'];
@@ -285,7 +332,7 @@ Zombie.prototype.randomZombiePos = function () {
 //zombies creation function
 Zombie.prototype.ZombiesCreation = function () {
     var ZombiesObj = document.createElement("div");
-    ZombiesObj.style.left = "1000px"; //#Map width-zombie width pic
+    ZombiesObj.style.left = "1225px"; //#Map width-zombie width pic
     ZombiesObj.style.top = this.randomZombiePos();
     ZombiesObj.classList.add(this.zombieStyle);
     ZombiesObj.setAttribute("id", "ZombiesSoldier" + Zombie.ZombieCount);
@@ -293,26 +340,22 @@ Zombie.prototype.ZombiesCreation = function () {
     Zombie.ZombiesArr.push(ZombiesObj.id);
     this.ZombiesMovement(ZombiesObj.id);
 };
-//-60
-var HouseLoss = 0; //zombie reached our home aand we lost the game -60
+var HouseLoss = -60;
 //creating a functiom to move the zombie till a condition is met
 Zombie.prototype.ZombiesMovement = function (_ZombiesObjID) {
     //timer for zombie movement;
     var that = this;
     that.ZombieTimer = setInterval(function () {
         $("#" + _ZombiesObjID).css("left", that.ZombieLeftMovement);
-        /*
-        if (that.blood == 0) {
-            clearInterval(that.ZombieTimer);
-            that.ZombiesRemove(_ZombiesObjID);
-        }
-        */
-        if ($("#" + _ZombiesObjID).position().left <= HouseLoss) {
-            // we  need to open the door and call game over function
-            clearInterval(that.ZombieTimer);
-            that.ZombiesRemove(_ZombiesObjID);
+        if ((Zombie.ZombiesArr.includes(_ZombiesObjID)) && ($("#" + _ZombiesObjID).position().left <= HouseLoss)) {
+        var x=$("#" + _ZombiesObjID).position().top;
+        var index = Zombie.ZombiesArr.indexOf(_ZombiesObjID);
+        clearInterval(objectsZombies[index].ZombieTimer);
+        objectsZombies[index].ZombiesRemove(_ZombiesObjID);
+        LawnRemoverMover(x);
         }
     }, that.walkSpeed);
+    
 };
 //creating a function to remove the zombie if a condition is met
 Zombie.prototype.ZombiesRemove = function (_ZombiesObjID) {
@@ -325,7 +368,7 @@ Zombie.prototype.ZombiesRemove = function (_ZombiesObjID) {
         "background-image": "url(./images/Zombie2/ZombieHead.gif)"
     });
     else if (bg_img.includes("Zombie3/0Zombie3.gif")) $("#" + _ZombiesObjID).css({
-        "background-image": "url(./images/Zombie3/ZombieDie.gif)"
+        "background-image": "url(./images/Zombie2/ZombieHead.gif)"
     });
     else if (bg_img.includes("Zombie4/0Zombie4.gif")) $("#" + _ZombiesObjID).css({
         "background-image": "url(./images/Zombie4/FlagZombieLostHead.gif)"
@@ -335,9 +378,11 @@ Zombie.prototype.ZombiesRemove = function (_ZombiesObjID) {
     });
     var zombieIdIndex = Zombie.ZombiesArr.indexOf(_ZombiesObjID)
     Zombie.ZombiesArr.splice(zombieIdIndex, 1);
+    objectsZombies.splice(zombieIdIndex, 1);
+    if(zombieNumber==0&&objectsZombies.length==0)GameWon();
     setTimeout(function () {
         $("#" + _ZombiesObjID).remove();
-        objectsZombies.splice(zombieIdIndex, 1);
+
     }, 2000)
 };
 //function to return random Zombies solider Class 
@@ -352,52 +397,138 @@ function PushZombiesoliderClass() {
 };
 //function to return the health of the created zombie
 function ZombieObjHealth(ZombieObjClass) {
-    if (ZombieObjClass == 'ZombiesSoldier1') return 10;
-    else if (ZombieObjClass == 'ZombiesSoldier2') return 15;
-    else if (ZombieObjClass == 'ZombiesSoldier3') return 20;
-    else if (ZombieObjClass == 'ZombiesSoldier4') return 25;
-    else if (ZombieObjClass == 'ZombiesSoldier5') return 30;
+    if (ZombieObjClass == 'ZombiesSoldier1') return 9;
+    else if (ZombieObjClass == 'ZombiesSoldier2') return 11;
+    else if (ZombieObjClass == 'ZombiesSoldier3') return 13;
+    else if (ZombieObjClass == 'ZombiesSoldier4') return 15;
+    else if (ZombieObjClass == 'ZombiesSoldier5') return 17;
 };
 //function to return the speed of the created zombie
 function ZombieObjSpeed(ZombieObjClass) {
-    if (ZombieObjClass == 'ZombiesSoldier1') return 300;
-    else if (ZombieObjClass == 'ZombiesSoldier2') return 350;
+    if (ZombieObjClass == 'ZombiesSoldier1') return 400;
+    else if (ZombieObjClass == 'ZombiesSoldier2') return 400;
     else if (ZombieObjClass == 'ZombiesSoldier3') return 400;
-    else if (ZombieObjClass == 'ZombiesSoldier4') return 500;
-    else if (ZombieObjClass == 'ZombiesSoldier5') return 550;
+    else if (ZombieObjClass == 'ZombiesSoldier4') return 400;
+    else if (ZombieObjClass == 'ZombiesSoldier5') return 400;
 };
 
 function ZombieObjBounty(ZombieObjClass) {
-    if (ZombieObjClass == 'ZombiesSoldier1') return 5;
-    else if (ZombieObjClass == 'ZombiesSoldier2') return 10;
-    else if (ZombieObjClass == 'ZombiesSoldier3') return 20;
-    else if (ZombieObjClass == 'ZombiesSoldier4') return 30;
-    else if (ZombieObjClass == 'ZombiesSoldier5') return 50;
+    if (ZombieObjClass == 'ZombiesSoldier1') return 0;
+    else if (ZombieObjClass == 'ZombiesSoldier2') return 0;
+    else if (ZombieObjClass == 'ZombiesSoldier3') return 0;
+    else if (ZombieObjClass == 'ZombiesSoldier4') return 0;
+    else if (ZombieObjClass == 'ZombiesSoldier5') return 0;
 };
 //////////////start the game for zombies ////////////////////////////////////////
 var Timer = 0; //Timer To  push new Zombie Class
 var Time = {
-    twentyseconds: 4
-    , fortySeconds: 8
-    , sixtySeconds: 12
-    , eightySeconds: 16
+    twentyseconds: 4,
+    fortySeconds: 8,
+    sixtySeconds: 12,
+    eightySeconds: 16,
+    TwoMinutes: 30
 };
 var objectsZombies = [];
-
+var flag = 1;
+var zombieNumber;
 function ZombieStart() {
     var ZombieObjClass = randomZombiesoliderClass();
     var ZombieHealth = ZombieObjHealth(ZombieObjClass);
     var ZombieSpeed = ZombieObjSpeed(ZombieObjClass);
     var ZombieBounty = ZombieObjBounty(ZombieObjClass);
-    var ZombiesTimer = 5000;
-    if (Timer == Time.eightySeconds) ZombiesTimer = 3000;
+    var ZombiesTimer;
+
+    if(Timer<Time.eightySeconds)ZombiesTimer = 5000;
+    else if (Timer < Time.TwoMinutes) ZombiesTimer = 3000;
+    else ZombiesTimer = 500;
     var ZombieObj = new Zombie(ZombieHealth, ZombieSpeed, ZombieObjClass, ZombieBounty);
-    ZombieObj.ZombiesCreation();
-    Timer++
+    objectsZombies.push(ZombieObj); //push object in the array
+    if (flag == 1) ZombieObj.ZombiesCreation();
+    Timer++;
     if (Timer == Time.twentyseconds || Timer == Time.fortySeconds || Timer == Time.sixtySeconds || Timer == Time.eightySeconds) {
         PushZombiesoliderClass();
     };
-    objectsZombies.push(ZombieObj); //push object in the array
-    setTimeout(ZombieStart, ZombiesTimer)
+    Zombie.ZombieCount++;
+     zombieNumber = 1000 - Zombie.ZombieCount;
+    document.getElementById("ZombieCount").innerHTML = 'Zombie = ' + zombieNumber;
+
+    if (zombieNumber > 0 && flag == 1) {
+        setTimeout(ZombieStart, ZombiesTimer);
+    };
+};
+// this function to give concstant increase in the score
+var FixedSunTimer;
+function FixedScoreValue(){
+        if(getFunds() ==400)clearTimeout(FixedSunTimer);
+        else changeFunds(Sun.cost);
+        FixedSunTimer=setTimeout(FixedScoreValue,5000);
+};
+//var TructorIndex=[];
+function LawnRemoverMover(ZombieTop) {
+//    if (TructorIndex.includes(ZombieTop)) {
+//        GameOver();
+//        return 0;
+//    };
+//    TructorIndex.push(ZombieTop);
+    var TractorLane = getTractorLane(ZombieTop);
+    console.log($(".LawnRemover").eq(TractorLane).position().left);
+    if($(".LawnRemover").eq(TractorLane).position().left<1000){
+    var TractorTimer=setInterval(function () {
+        $(".LawnRemover").eq(TractorLane).css({
+            "left": "+=10px"
+        });
+        var indexArray = [];
+        for (var i = 0; i < objectsZombies.length; i++) {
+            if ($("#" + Zombie.ZombiesArr[i]).position().top == ZombieTop) {
+                if (($(".LawnRemover").eq(TractorLane).position().left + 50)<=($("#" + Zombie.ZombiesArr[i]).position().left) <= ($(".LawnRemover").eq(TractorLane).position().left + 75)) {
+                    indexArray.push(i);
+                }
+            }
+        };
+        for (var i = indexArray.length-1; i >= 0; i--) {
+            clearInterval(objectsZombies[indexArray[i]].ZombieTimer);
+            objectsZombies[indexArray[i]].ZombiesRemove(Zombie.ZombiesArr[indexArray[i]]);
+        }
+    if($(".LawnRemover").eq(TractorLane).position().left>1300){
+        clearInterval(TractorTimer);
+        //document.getElementsByClassName("LawnRemover")[TractorLane].style.display = "none";
+    }
+    }, 1);
 }
+    else GameOver();
+};
+
+function GameWon() {
+    $("#dialogWon").dialog({
+        width: 500,
+        show: {
+            effect: "explode",
+            duration: 1000
+        },
+    });
+    clearInterval(FixedSunTimer);
+}
+function GameOver() {
+    $("#dialog").dialog({
+        width: 800,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+    });
+    $("#dialog").show();
+    for (var i = 0; i < objectsZombies.length; i++) {
+        clearInterval(objectsZombies[i].ZombieTimer);
+        flag = 0;
+    };
+    pShooters.forEach(function (shooter) {
+        $(shooter.gridObject).children().stop(true).remove();
+        shooter.stopShooting();
+    });
+    clearInterval(int1);
+    clearInterval(FixedSunTimer);
+};
+$("#dialog").hide();
+$("#dialogWon").hide();
 ZombieStart();
+ FixedScoreValue();
